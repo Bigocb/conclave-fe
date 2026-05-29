d97a532c10e606c49579c0e418f440db6c2bf5dc
bigocb <bobby-cloutier@gmail.com>
2026-05-29 12:02:03 -0400

    fix: add missing PulseProvider to resolve runtime crash

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './hooks/useAuth'
+import { PulseProvider } from './hooks/usePulse'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <App />
+        <PulseProvider>
+          <App />
+        </PulseProvider>
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
